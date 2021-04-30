3fa02aa17967792f003fe5e0c906be5bfc17a015
TacticsDreams <TacticsDreams@users.noreply.github.com>
2021-04-30 17:15:34 +0200

    Navbar modifiée + UI changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+var fileName = location.href.split("/").slice(-1); 
 
 
 const footer = document.querySelector(".footer__link");
@@ -20,3 +21,20 @@ dark.addEventListener('click', (e) => {
     }
 });
 
+
+const navUL = document.querySelector(".topheader__navlist");
+let burger = document.querySelector('.burger');
+burger.addEventListener('click', (e) => {
+    navUL.classList.toggle("show");
+    burger.classList.toggle("show");
+});
+
+if (fileName[0].includes("main.html")) {
+    fetch('assets/data/data.json').then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        document.querySelector('.article__header').innerHTML = data[0].title;
+        document.querySelector('.article__sub').innerHTML = data[0].sub;
+        document.querySelector('.article__link').innerHTML = data[0].website.split("/").slice(2, 3);
+    });
+}
