821459a27c6a9a214732e6e67ed7dc4fc3653757
TacticsDreams <TacticsDreams@users.noreply.github.com>
2021-05-04 18:52:45 +0200

    Edition du SASS, JS, et implémentations d'archives

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -30,34 +30,43 @@ burger.addEventListener('click', (e) => {
 });
 
 if (fileName[0].includes("main.html")) {
-    const day = new Date(2021, 4, 29);
+    const day = new Date(2021, 3, 29);
     const newsBlock = document.querySelector(".news");
     const timelist = document.querySelectorAll(".timelist__el");
     const milliseconds = 1000*60*60*24;
 
-    let localTM = window.localStorage.getItem("MI_TimeMachine");
+    let localTM = window.sessionStorage.getItem("MI_TimeMachine");
     if (localTM != undefined) {
-        newsBlock.setAttribute("data-day", window.localStorage.getItem("MI_TimeMachine"));
+        newsBlock.setAttribute("data-day", window.sessionStorage.getItem("MI_TimeMachine"));
     } else {
-        window.localStorage.setItem('MI_TimeMachine', "4");
+        window.sessionStorage.setItem('MI_TimeMachine', "4");
     }
 
-    for (let i = 0; i<timelist.length; i++) {
-        timelist[i].innerHTML = (day.getDate() - (4 - (i + 1)));
-    }
+
 
     let timestamp = parseInt(document.querySelector(".news").getAttribute("data-day"));
     let currentDay = new Date(day - (4 - timestamp));
-    let dayString = currentDay.getDate() + "/" + currentDay.getMonth() + "/" + currentDay.getFullYear();
+    let monthCorrection = currentDay.getMonth() + 1;
+    if (monthCorrection.toString().length === 1) monthCorrection = "0" + monthCorrection;
+    let dayString = currentDay.getDate() + "/" + monthCorrection + "/" + currentDay.getFullYear();
     document.querySelector(".timestamp").innerHTML = dayString;
+    
+    for (let i = 0; i<timelist.length; i++) {
+        timelist[i].innerHTML = (day.getDate() - (4 - (i + 1)));
+        timelist[i].addEventListener("click", () => {
+            timestamp = i + 1;
+            updateTime();
+            buttonCheck();
+            generateArticles();
+        });
+    }
 
     function updateTime() {
         newsBlock.setAttribute("data-day", timestamp);
-        window.localStorage.setItem("MI_TimeMachine", timestamp);
-        window.localStorage.setItem('MI_TimeMachine', timestamp);
+        window.sessionStorage.setItem("MI_TimeMachine", timestamp);
         // Je dois vraiment convertir en millisecondes?
         currentDay = new Date(day - (4 - timestamp) * milliseconds);
-        dayString = currentDay.getDate() + "/" + currentDay.getMonth() + "/" + currentDay.getFullYear();
+        dayString = currentDay.getDate() + "/" + monthCorrection + "/" + currentDay.getFullYear();
         document.querySelector(".timestamp").innerHTML = dayString;
         /**
         * Dans la time machine, permet d'afficher actif un jour précis.
@@ -169,4 +178,66 @@ if (fileName[0].includes("main.html")) {
     buttonCheck();
     updateTime();
     generateArticles();
+}
+
+if (fileName[0].includes("archives.html")) {
+    const form = document.querySelector(".searchForm");
+    const resultDisplay = document.querySelector('#results');
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let searchDate = new Date(document.querySelector("#searchDate").value);
+        let monthCorrection = searchDate.getMonth() + 1;
+        if (monthCorrection.toString().length === 1) monthCorrection = "0" + monthCorrection;
+        let dayString = searchDate.getDate() + "/" + monthCorrection + "/" + searchDate.getFullYear();
+
+        fetch('assets/data/data.json').then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            // Factory reset.
+            let clippings = document.querySelectorAll(".article");
+            clippings.forEach(function (clip) {
+                document.querySelector(".archives").removeChild(clip);
+            });
+
+            let counter = 0;
+            for (let i = 0; i<data.length; i++) {
+                if (data[i].date == dayString) {
+                    counter++;
+
+                    
+                    let container = document.createElement("article");
+                    let header = document.createElement("h2");
+                    let subheader = document.createElement("p");
+                    let link = document.createElement("p");
+    
+                    container.classList.add("article");
+                    if (data[i].priority == "true") container.classList.add("priority");
+                    container.addEventListener("click", (e) => {
+                        location.href = data[i].website;
+                    });
+    
+                    header.classList.add("article__header");
+                    header.innerHTML = data[i].title;
+                    container.appendChild(header);
+    
+                    subheader.classList.add("article__sub");
+                    subheader.innerHTML = data[i].sub;
+                    container.appendChild(subheader);
+    
+                    link.classList.add("article__link");
+                    link.innerHTML = data[i].website.split("/").slice(2, 3);
+                    container.appendChild(link);
+    
+                    document.querySelector('.archives').appendChild(container);
+                }
+            }
+            if (counter > 0) {
+                resultDisplay.innerHTML = "Trouvé " + counter + " articles du " + dayString + ".";
+            } else {
+                resultDisplay.innerHTML = "Aucun article trouvé pour le " + dayString + ".";
+            }
+
+
+        });
+    });
 }
